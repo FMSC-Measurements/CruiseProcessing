2dc7989bf2fb9d6db3e79ae54b16e17e8f8f95cf
Ben Campbell <benjocamps@gmail.com>
2024-05-17 12:07:25 -0500

    [FIX] #38 MerchHeightSecondary not calculating volumes

--- src/CruiseProcessing.Core.Test/CalculateTreeValues_Test.cs
+++ src/CruiseProcessing.Core.Test/CalculateTreeValues_Test.cs
@@ -63,12 +63,110 @@ namespace CruiseProcessing.Test
 
         [InlineData("Version3Testing\\27504PCM_Spruce East_Timber_Sale.cruise")]
         [InlineData("Version3Testing\\99996FIX_PNT_Timber_Sale_08242021.cruise")]
-        public void ProcessTrees(string fileName)
+        [InlineData("Issues\\20383_Jiffy Stewardship_TS.04.30.24.process")]
+
+
+        public void ProcessTrees_Compare_CalulateTreeValues2(string fileName)
         {
             var filePath = GetTestFile(fileName);
 
             var mockLogger = Substitute.For<ILogger<CpDataLayer>>();
             var dal = new DAL(filePath);
+            var dataLayer = new CpDataLayer(dal, mockLogger);
+            var ctv = new CalculateTreeValues(dataLayer);
+
+            var trees = dataLayer.getTrees();
+            trees.All(x => x.TreeDefaultValue_CN != null && x.TreeDefaultValue_CN > 0)
+                .Should().BeTrue();
+
+            dataLayer.DeleteLogStock();
+            dataLayer.deleteTreeCalculatedValues();
+
+            var strata = dataLayer.GetStrata();
+            dal.BeginTransaction();
+            foreach (var st in strata)
+            {
+                ctv.ProcessTrees(st.Code, st.Method, st.Stratum_CN.Value);
+            }
+            dal.CommitTransaction();
+
+            var tcvs = dataLayer.getTreeCalculatedValues();
+            var logStocks = dataLayer.getLogStock();
+
+
+            var ctv2 = new CalculateTreeValues2(dataLayer);
+
+            dataLayer.DeleteLogStock();
+            dataLayer.deleteTreeCalculatedValues();
+            dal.BeginTransaction();
+            foreach (var st in strata)
+            {
+                ctv2.ProcessTrees(st.Code, st.Method, st.Stratum_CN.Value);
+            }
+            dal.CommitTransaction();
+
+            var tcvs2 = dataLayer.getTreeCalculatedValues();
+            var logStocks2 = dataLayer.getLogStock();
+
+
+            tcvs2.Count.Should().Be(tcvs.Count);
+            logStocks2.Count.Should().Be(logStocks.Count);
+
+            foreach (var pairTcv in tcvs2.Zip(tcvs, (x, y) => (x, y)))
+            {
+                pairTcv.x.Should().BeEquivalentTo(pairTcv.y,
+                    config: cfg => cfg
+                    .Excluding(x => x.rowID)
+                    .Excluding(x => x.TreeCalcValues_CN)
+                    .Excluding(x => x.Self)
+                    .Excluding(x => x.DAL)
+                    .Excluding(x => x.Tree));
+            }
+
+            var logStockTrees = logStocks.GroupBy(x => x.Tree_CN).ToDictionary(x => x.Key, y => y.ToArray());
+            var logStockTrees2 = logStocks2.GroupBy(x => x.Tree_CN).ToDictionary(x => x.Key, y => y.ToArray());
+
+            foreach (var tcn in logStockTrees.Keys)
+            {
+                logStockTrees2.ContainsKey(tcn).Should().BeTrue();
+
+                var logs = logStockTrees[tcn];
+                var logs2 = logStockTrees2[tcn];
+
+                logs.Length.Should().Be(logs2.Length);
+
+                foreach (var item in logs2.Select((log, i) => (log, i)))
+                {
+                    item.log.Should().BeEquivalentTo(logs[item.i],
+                        config: cfg => cfg
+                        .Excluding(x => x.rowID)
+                        .Excluding(x => x.LogStock_CN)
+                        .Excluding(x => x.Self)
+                        .Excluding(x => x.DAL)
+                        .Excluding(x => x.Tree));
+                }
+            }
+
+
+        }
+
+        [Theory]
+        [InlineData("Issues\\20383_Jiffy Stewardship_TS.crz3")]
+        public void ProcessTrees_Compare_CalulateTreeValues2_V3(string fileName)
+        {
+            var filePath = GetTestFile(fileName);
+
+            var mockLogger = Substitute.For<ILogger<CpDataLayer>>();
+
+            var db = new CruiseDatastore_V3(filePath);
+            var cruiseID = db.From<CruiseDAL.V3.Models.Cruise>().Query().Single().CruiseID;
+
+            var v2Path = GetTempFilePath(".cruise", Path.GetFileNameWithoutExtension(filePath) + ".v2");
+            var dal = new DAL(v2Path, true);
+
+            var migrator = new DownMigrator();
+            migrator.MigrateFromV3ToV2(cruiseID, db, dal);
+
             var dataLayer = new CpDataLayer(dal, mockLogger);
             var ctv = new CalculateTreeValues(dataLayer);
 
