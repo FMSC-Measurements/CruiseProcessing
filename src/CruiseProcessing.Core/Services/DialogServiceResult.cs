9fb050ef1c9bc5f8e5d3d91322ffeff3d54135d6
Benjamin J. Campbell <Benjamin.campbell@usda.gov>
2024-08-09 14:34:41 -0500

    Add DialogService.AskYesNo

---
+++ src/CruiseProcessing.Core/Services/DialogServiceResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CruiseProcessing.Services
+{
+    public enum DialogServiceResult
+    {
+        Yes,
+        No,
+        Cancel,
+        Ok,
+        Abort,
+        Retry,
+        Ignore,
+    }
+}
