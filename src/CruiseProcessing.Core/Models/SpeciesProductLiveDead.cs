e0006b6dfd5828c7d73461c787696df6699b3c5f
Ben Campbell <benjocamps@gmail.com>
2024-08-15 16:34:18 -0500

    enable biomass equation generation for all L/D values in a species/product

---
+++ src/CruiseProcessing.Core/Models/SpeciesProductLiveDead.cs
@@ -0,0 +1,10 @@
+﻿namespace CruiseProcessing.Models
+{
+    public class SpeciesProductLiveDead
+    {
+        public string SpeciesCode { get; set; }
+        public int FiaCode { get; set; }
+        public string ProductCode { get; set; }
+        public string LiveDead { get; set; }
+    }
+}
--- src/CruiseProcessing.Test/VolumeEquations_Test.cs
+++ src/CruiseProcessing.Test/VolumeEquations_Test.cs
@@ -10,6 +10,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit.Abstractions;
 
+using TreeDefaultValue = CruiseDAL.V2.Models.TreeDefaultValue;
+
 namespace CruiseProcessing.Test
 {
     public class VolumeEquations_Test : DiTestBase
@@ -34,5 +36,109 @@ namespace CruiseProcessing.Test
             result.Where(x => x.Component == "SecondaryProd").Single().WeightFactorSecondary.Should().BeGreaterThan(0);
         }
 
+
+        [Fact]
+        public void UpdateBiomassCruise()
+        {
+            var treeDefaults = new[]
+{
+                new TreeDefaultValue{ Species = "101", FIAcode = 101, PrimaryProduct = "01", LiveDead = "L" },
+                new TreeDefaultValue{ Species = "101", FIAcode = 101, PrimaryProduct = "01", LiveDead = "D" },
+                new TreeDefaultValue{ Species = "102", FIAcode = 102, PrimaryProduct = "01", LiveDead = "L" },
+            };
+
+            var init = new DatabaseInitializer_V2()
+            {
+                TreeDefaults = treeDefaults,
+            };
+
+            using var db = init.CreateDatabase();
+
+            var trees = treeDefaults.Select((tdv, i) => MakeTree(i + 1, tdv)).ToArray();
+
+            foreach(var tree in trees)
+            {
+                db.Insert(tree);
+            }
+
+
+            var dataLayer = new CpDataLayer(db, Substitute.For<ILogger<CpDataLayer>>(), true);
+
+            var volumeEquations = new[]
+            {
+                new VolumeEquationDO{ CalcBiomass = 1, Species = "101", PrimaryProduct = "01", VolumeEquationNumber = "1234567890" },
+                new VolumeEquationDO{ CalcBiomass = 1, Species = "102", PrimaryProduct = "01", VolumeEquationNumber = "02" },
+            };
+
+            var PRs = new[]
+            {
+                new PercentRemoved{ bioSpecies = "101", bioProduct = "01", bioPCremoved = "95.0" },
+                new PercentRemoved{ bioSpecies = "102", bioProduct = "01", bioPCremoved = "96.0" },
+            };
+
+            VolumeEquations.UpdateBiomassTemplate(dataLayer, volumeEquations, "01", "01", PRs);
+
+            var biomassEqsAgain = db.From<CruiseDAL.V2.Models.BiomassEquation>().Query().ToArray();
+
+            biomassEqsAgain.Should().HaveCountGreaterOrEqualTo(volumeEquations.Length * VolumeEquations.BIOMASS_COMPONENTS.Count);
+            biomassEqsAgain.Should().HaveCountLessOrEqualTo(2 * volumeEquations.Length * VolumeEquations.BIOMASS_COMPONENTS.Count);
+
+            biomassEqsAgain.Where(x => x.Species == "101" && x.Product == "01" && x.LiveDead == "L" && x.Component == "PrimaryProd").Should().HaveCount(1);
+            biomassEqsAgain.Where(x => x.Species == "101" && x.Product == "01" && x.LiveDead == "D" && x.Component == "PrimaryProd").Should().HaveCount(1);
+
+
+            TreeDO MakeTree(int treeNumber, TreeDefaultValue tdv)
+            {
+                return new TreeDO
+                {
+                    TreeNumber = treeNumber,
+                    CuttingUnit_CN = 1,
+                    Stratum_CN = 1,
+                    SampleGroup_CN = 1,
+                    Species = tdv.Species,
+                    LiveDead = tdv.LiveDead,
+                    TreeDefaultValue_CN = tdv.TreeDefaultValue_CN ?? throw new ArgumentException("expected tdv TreeDefaultValue_CN to not be null"),
+                };
+            }
+        }
+
+        [Fact]
+        public void UpdateBiomassTemplate()
+        {
+            var treeDefaults = new[]
+            {
+                new TreeDefaultValue{ Species = "101", FIAcode = 101, PrimaryProduct = "01", LiveDead = "L" },
+                new TreeDefaultValue{ Species = "102", FIAcode = 102, PrimaryProduct = "01", LiveDead = "L" },
+            };
+
+            var init = new DatabaseInitializer_V2()
+            {
+                TreeDefaults = treeDefaults,
+            };
+
+            using var db = init.CreateDatabase();
+
+            var dataLayer = new CpDataLayer(db, Substitute.For<ILogger<CpDataLayer>>(), true);
+
+            var volumeEquations = new[]
+            {
+                new VolumeEquationDO{ CalcBiomass = 1, Species = "101", PrimaryProduct = "01", VolumeEquationNumber = "1234567890" },
+                new VolumeEquationDO{ CalcBiomass = 1, Species = "102", PrimaryProduct = "01", VolumeEquationNumber = "02" },
+            };
+
+            var PRs = new[]
+            {
+                new PercentRemoved{ bioSpecies = "101", bioProduct = "01", bioPCremoved = "95.0" },
+                new PercentRemoved{ bioSpecies = "102", bioProduct = "01", bioPCremoved = "96.0" },
+            };
+
+            VolumeEquations.UpdateBiomassTemplate(dataLayer, volumeEquations, "01", "01", PRs);
+
+            var biomassEqsAgain = db.From<CruiseDAL.V2.Models.BiomassEquation>().Query().ToArray();
+
+            biomassEqsAgain.Should().HaveCount(volumeEquations.Length * VolumeEquations.BIOMASS_COMPONENTS.Count);
+
+
+        }
     }
 }
