f5848e9817659e7b9895499f07c495a055b710c9
Ben Campbell <benjocamps@gmail.com>
2024-12-03 21:26:32 -0600

    update biomass equations before test ProcessCruise_CompareImplimentations_TreeCalculatedValues

--- src/CruiseProcessing.Core.Test/Processing/CruiseProcessor_Test.cs
+++ src/CruiseProcessing.Core.Test/Processing/CruiseProcessor_Test.cs
@@ -19,7 +19,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit.Abstractions;
 
-namespace CruiseProcessing.Test
+namespace CruiseProcessing.Test.Processing
 {
     public class CruiseProcessor_Test : TestBase
     {
@@ -342,44 +342,44 @@ namespace CruiseProcessing.Test
         [Theory]
         //[InlineData("OgTest\\BLM\\Hammer Away.cruise")]
         //[InlineData("OgTest\\BLM\\Long Nine.cruise")]
-        [InlineData("OgTest\\Region1\\R1_FrenchGulch.cruise")]
-        [InlineData("OgTest\\Region2\\R2_Test.cruise")]
-        [InlineData("OgTest\\Region2\\R2_Test_V3.process")]
+        //[InlineData("OgTest\\Region1\\R1_FrenchGulch.cruise")]
+        //[InlineData("OgTest\\Region2\\R2_Test.cruise")]
+        //[InlineData("OgTest\\Region2\\R2_Test_V3.process")]
         //[InlineData("OgTest\\Region3\\R3_FCM_100.cruise")]
         //[InlineData("OgTest\\Region3\\R3_PCM_FIXCNT.cruise")]
         [InlineData("OgTest\\Region3\\R3_PNT_FIXCNT.cruise")]
-        [InlineData("OgTest\\Region4\\R4_McDougal.cruise")]
+        //[InlineData("OgTest\\Region4\\R4_McDougal.cruise")]
         [InlineData("OgTest\\Region5\\R5.cruise")]
         //[InlineData("OgTest\\Region6\\R6.cruise")]
         [InlineData("OgTest\\Region8\\R8.cruise")]
-        [InlineData("OgTest\\Region9\\R9.cruise")]
-        [InlineData("OgTest\\Region10\\R10.cruise")]
+        //[InlineData("OgTest\\Region9\\R9.cruise")]
+        //[InlineData("OgTest\\Region10\\R10.cruise")]
 
-        [InlineData("Version3Testing\\3P\\87654 test 3P TS.cruise")]
+        //[InlineData("Version3Testing\\3P\\87654 test 3P TS.cruise")]
         //[InlineData("Version3Testing\\3P\\87654_test 3P_Timber_Sale_26082021.process")]
         //[InlineData("Version3Testing\\3P\\87654_test 3P_Timber_Sale_26082021_fixedTallyBySp.process")]
-        [InlineData("Version3Testing\\3P\\87654_Test 3P_Timber_Sale_30092021.process")]
+        //[InlineData("Version3Testing\\3P\\87654_Test 3P_Timber_Sale_30092021.process")]
 
-        [InlineData("Version3Testing\\FIX\\20301 Cold Springs Recon.cruise")]
+        //[InlineData("Version3Testing\\FIX\\20301 Cold Springs Recon.cruise")]
         //[InlineData("Version3Testing\\FIX\\20301_Cold Springs_Timber_Sale_29092021.process")]
 
         //[InlineData("Version3Testing\\FIX and PNT\\99996_TestMeth_Timber_Sale_08072021.process")]
 
-        [InlineData("Version3Testing\\PCM\\27504_Spruce East_TS.cruise")]
+        //[InlineData("Version3Testing\\PCM\\27504_Spruce East_TS.cruise")]
 
-        [InlineData("Version3Testing\\PNT\\Exercise3_Dead_LP_Recon.cruise")]
+        //[InlineData("Version3Testing\\PNT\\Exercise3_Dead_LP_Recon.cruise")]
 
         //[InlineData("Version3Testing\\STR\\98765 test STR TS.cruise")]
         //[InlineData("Version3Testing\\STR\\98765_test STR_Timber_Sale_26082021.process")]
-        [InlineData("Version3Testing\\STR\\98765_test STR_Timber_Sale_30092021.process", Skip = "Has Blank Tree Grades")]
+        //[InlineData("Version3Testing\\STR\\98765_test STR_Timber_Sale_30092021.process", Skip = "Has Blank Tree Grades")]
 
         //[InlineData("Version3Testing\\TestMeth\\99996_TestMeth_TS_202310040107_KC'sTabActive3-R9Q8.process")]
 
-        [InlineData("Version3Testing\\27504PCM_Spruce East_Timber_Sale.cruise")]
-        [InlineData("Version3Testing\\99996FIX_PNT_Timber_Sale_08242021.cruise")]
+        //[InlineData("Version3Testing\\27504PCM_Spruce East_Timber_Sale.cruise")]
+        //[InlineData("Version3Testing\\99996FIX_PNT_Timber_Sale_08242021.cruise")]
 
-        [InlineData("Temp\\21853\\21853 Swanson Creek TS Data Checked.cruise")]
-        [InlineData("Temp\\21853\\21853 Swanson Creek TS Data Checked_withTallySetup.cruise")]
+        //[InlineData("Temp\\21853\\21853 Swanson Creek TS Data Checked.cruise")]
+        //[InlineData("Temp\\21853\\21853 Swanson Creek TS Data Checked_withTallySetup.cruise")]
         public void ProcessCruise_CompareImplimentations_TreeCalculatedValues(string testFileName)
         {
             //
@@ -455,7 +455,7 @@ namespace CruiseProcessing.Test
                 new CruiseProcessorItem
                 {
                     Name = "NewProcess_20241101",
-                    Processor = new CruiseProcessor_20241101_Preview(dataLayer, Substitute.For<IDialogService>(), new CalculateTreeValues_20241101(dataLayer, new VolumeLibrary_20241101(), CreateLogger<CalculateTreeValues_20241101>()), CreateLogger<CruiseProcessor_20241101_Preview>())
+                    Processor = new CruiseProcessor_20241101_Preview(dataLayer, Substitute.For<IDialogService>(), new CalculateTreeValues3(dataLayer, new VolumeLibrary_20241101(), CreateLogger<CalculateTreeValues3>()), CreateLogger<CruiseProcessor_20241101_Preview>())
                 },
                 new CruiseProcessorItem
                 {
@@ -465,9 +465,11 @@ namespace CruiseProcessing.Test
                 new CruiseProcessorItem
                 {
                     Name = "NewProcess_20241115",
-                    Processor = new CruiseProcessor_20241101_Preview(dataLayer, Substitute.For<IDialogService>(), new CalculateTreeValues_20241101(dataLayer, new VolumeLibrary_20241115(), CreateLogger<CalculateTreeValues_20241101>()), CreateLogger<CruiseProcessor_20241101_Preview>())
+                    Processor = new CruiseProcessor_20241101_Preview(dataLayer, Substitute.For<IDialogService>(), new CalculateTreeValues3(dataLayer, new VolumeLibrary_20241115(), CreateLogger<CalculateTreeValues3>()), CreateLogger<CruiseProcessor_20241101_Preview>())
                 },
             };
+
+            BiomassHelpers.UpdateBiomass(dataLayer, dataLayer.getVolumeEquations());
 
             foreach (var processor in processors)
             {
