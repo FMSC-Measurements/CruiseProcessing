787fc06d654848f558451b7b3d44be998ef37015
Benjamin J. Campbell <Benjamin.campbell@usda.gov>
2024-12-05 21:32:44 -0600

    WIP working on defining schema for TEA report

---
+++ src/CruiseProcessing.Test/OutputModels/TeaReport_Test.cs
@@ -0,0 +1,30 @@
+﻿using CruiseProcessing.OutputModels;
+using Json.Schema;
+using Json.Schema.Generation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace CruiseProcessing.Test.OutputModels
+{
+    public class TeaReport_Test : TestBase
+    {
+        public TeaReport_Test(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void GenerateSchema()
+        {
+            var schemabulider = new JsonSchemaBuilder();
+            var schema = schemabulider.FromType<TeaReport>().Build();
+
+            var schemaString = JsonSerializer.Serialize(schema, new JsonSerializerOptions { WriteIndented = true, IndentSize = 4});
+            Output.WriteLine(schemaString);
+        }
+    }
+}
